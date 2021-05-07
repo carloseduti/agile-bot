2c28319b01d6612af3b847db563bb5d30798a232
Carlos Eduardo F da Costa <carlos.costa@cf.coop.br>
2021-05-07 01:10:36 -0300

    implementando camada de serviço e chamando no bot controller

--- src/app/controllers/alunoController.js
+++ src/app/controllers/alunoController.js
@@ -1,60 +1,44 @@
-const Aluno = require('../models/Aluno');
+const AlunoServices = require('../services/alunoService');;
 
 class AlunoController {
 
     async findAll(req, res) {
-        Aluno.find(function (error, alunos) {
-            if (error) {
-                res.send("Erro ao tentar selecionar todos os alunos", error)
-            }
-            return res.status(200).json({ alunos });
-        });
+        try {
+            const resultado = await new AlunoServices().findAlunoAll();
+            return res.status(200).json({ result: resultado })
+        } catch (error) {
+            return res.status(500).json({ message: 'error' + ' ' + error })
+        }
     }
 
     async findAlunoById(req, res) {
-        Aluno.findById(req.params.aluno_id, function (error, aluno) {
-            if (error || aluno == null) {
-                res.status(400).send("Id não encontrado!");
-            }
-            return res.status(200).json(aluno);
-        });
+        try {
+            const id = req.params.id
+            const resultado = await new AlunoServices().findAluno(id);
+            return res.status(200).json({ result: resultado })
+
+        } catch (error) {
+            return res.status(500).json({ message: 'error' + ' ' + error })
+        }
     }
 
     async createAluno(req, res) {
         try {
-            const alunoModel = new Aluno(req.body);
-            const resultado = await alunoModel.save(req.body);
-            return res.status(200).json({ resultado });
+            const resultado = await new AlunoServices().createAluno(req.body);
+            return res.status(200).json({ message: resultado })
         } catch (error) {
-            console.error(error)
-            return res.status(500).json({ error: "Erro ao criar aluno!" });
-        }
-    }
-
-    async updateAluno(req, res) {
-        try {
-            Aluno.findById(req.params.id, await function(error,aluno){
-                if (error || aluno == null) {
-                    res.status(400).send("Id não encontrado!");
-                }
-                const alunoModel = new Aluno(aluno);
-                const resultado = alunoModel.save(aluno);
-                return res.status(200).json({ resultado })
-            });
-         
-        } catch (error) {
-            console.log(error)
-            return res.status(400).json({ message: "Erro ao atualizar Aluno" })
+            return res.status(500).json({ message: "Erro ao criar aluno"+ ' ' + error})
         }
     }
 
     async deleteAlunoById(req, res) {
-        Aluno.deleteOne({ _id: req.params.aluno_id }, function (error) {
-            if (error) {
-                res.status(400).send("Id não encontrado!")
-            }
-            return res.status(200).json({ message: "Aluno Deletado com sucesso!" });
-        })
+        try {
+            const id = req.params.id
+            const resultado = await new AlunoServices().deleteAluno(id)
+            return res.status(200).json({ message: resultado});
+        } catch (error) {
+            return res.status(500).json({ messagem: "Erro ao deletar aluno" + '' + error})
+        }
     }
 }
 
