e016229b5e36bb2c56fa23921ee5da338a84eab4
Carlos Eduardo F da Costa <carlos.costa@cf.coop.br>
2021-05-11 14:03:37 -0300

    adicionando nova chave

--- src/app/controllers/botController.js
+++ src/app/controllers/botController.js
@@ -4,11 +4,11 @@ const alunoService = require('../services/alunoService');
 
 class BotController {
     async index() {
-      
+
         try {
 
             const token = process.env.TELEGRAM_TOKEN;
-            
+
             const bot = new TelegramBot(token, { polling: true });
 
             bot.on('message', async (msg) => {
@@ -24,16 +24,21 @@ class BotController {
                 // Texto a partir da resposta do dialogflow.
                 let responseText = dfResponse.text;
 
-                if (dfResponse.intent == 'intent_matricula') {
-                    const matricula = msg.text;
-                    const resultado = (await new alunoService().findAlunoByMatricula(matricula)).toObject();
-                    if (resultado) {
-                        bot.sendMessage(chatId, "Email enviado para: " + resultado.email);
-                    }
-               
-                }
-                // Envio da mensagem para o usuário do Telegram.
+                console.log("dfResponse ->", dfResponse)
+                console.log('Intent ->', dfResponse.intent)
+
                 bot.sendMessage(chatId, responseText);
+
+                if (dfResponse.intent == 'intent_boasvindas' && dfResponse.fields.matricula.numberValue != '') {
+                    const matricula = dfResponse.fields.matricula.numberValue;
+                    const resultado = await new alunoService().findAlunoByMatricula(matricula);
+                    if (resultado != null) {
+                        const aluno = resultado.toObject();
+                        bot.sendMessage(chatId, "Email enviado para: " + aluno.email);
+                    } else {
+                        bot.sendMessage(chatId, "Matricula não encontrada.");
+                    }
+                }
             });
 
         } catch (error) {
