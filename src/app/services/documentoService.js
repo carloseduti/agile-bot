b038bd638d72ed83ec39e21b3a88a159f84e47d7
Wanderson027 <w.rafael.s@hotmail.com>
2021-06-07 19:36:22 -0300

    ajuste corpo email e declaração

--- src/app/services/documentoService.js
+++ src/app/services/documentoService.js
@@ -1,26 +1,28 @@
 class DocumentoService {
     async conteudoDeclaracao(aluno) {
-        const conteudo = ` <div style="text-align: center;">
-        <div>
+        const conteudo = ` <div style="margin: 0 auto; width: 90%;">
+        <div style="text-align: center;">
             <div>
-                <img width="250px;" src="https://images.educamaisbrasil.com.br/content/superior/instituicao/logo/g/unidesc.png">
+                <div>
+                    <img width="150px;" src="https://images.educamaisbrasil.com.br/content/superior/instituicao/logo/g/unidesc.png">
+                </div>
+                <h3 style="font-size:14px">Centro Univesitário de Desenvolvimento do Centro Oeste</h3>
+            </div><br><br>
+            <div>
+                <h2></h2>
             </div>
-            <h1>Centro Univesitário de Desenvolvimento do Centro Oeste</h1>
+    
         </div>
-        <div>
-            <h2></h2>
-        </div>
-
-    </div>
-    <div style="text-align: center;">
-        <h1>Declaração de Matrícula</h1>
-        <div>
-            <p style="text-align: justify;">
-                Declaramos para os devidos fins, que o aluno(a) <b>${aluno.nome}</b>, portador do CPF: <b>${aluno.cpf}</b>, RG:
-                <b>${aluno.rg}</b> encontra-se regularmente matriculado no Centro Universitário de Desenvolvimento do
-                Centro Oeste sobe o nº <b>${aluno.matricula}</b> e está no <b>${aluno.semestre}</b> do curso de <b>${aluno.curso}</b>,
-                com inicio em <b>${aluno.inicio}</b> e previsão de término em <b>${aluno.conclusao}</b>.
-            </p>
+        <div style="text-align: center; margin: 0; text-indent: 3ch;">
+            <h3 style="font-size:14px">Declaração de Matrícula</h3>
+            <div><br>
+                <p style="text-align: justify; font-size:11px; line-height: 1.8;">
+                    Declaramos para os devidos fins, que o aluno(a) <b>${aluno.nome}</b>, portador do CPF: <b>${aluno.cpf}</b>, RG:
+                    <b>${aluno.rg}</b> encontra-se regularmente matriculado no Centro Universitário de Desenvolvimento do
+                    Centro Oeste sobe o nº <b>${aluno.matricula}</b> e está no <b>${aluno.semestre}</b> do curso de <b>${aluno.curso}</b>,
+                    com inicio em <b>${aluno.inicio}</b> e previsão de término em <b>${aluno.conclusao}</b>.
+                </p>
+            </div>
         </div>
     </div>
     <footer style="
@@ -31,9 +33,9 @@ class DocumentoService {
             bottom: 0;
             left: 0;"
             >
-        <h3>Luziânia-GO</h3>
+        <h3 style="font-size:14px">Luziânia-GO</h3>
         <div>
-            <h3>-Preencher data-</h3>
+            <h3 style="font-size:14px">07-Jun-2021</h3>
         </div>
     </footer>
          `;
@@ -43,17 +45,18 @@ class DocumentoService {
     async conteudoEmailDeclaracao(aluno) {
         const conteudo = ` 
         <div style="margin: 0 auto; width: 50%;">
-            <div style="text-align: center; background-color: rgba(35, 38, 216, 0.808); color: antiquewhite; height: 120px; line-height: 8px; border-radius: 5px">
-                    <img style="width: 60px; height: 6opx; margin-top: 10px;" src="https://static.crozdesk.com/web_app_library/providers/logos/000/003/155/original/chatbots-builder-1559230712-logo.png?1559230712">
-                    <h1 style="text-shadow: rgb(119, 116, 116) 0.1em 0.1em 0.2em"> Atendimento academico unidesc </h1>
+            <div style="text-align: center; background-color: rgba(1, 65, 241, 0.808); color: antiquewhite; height: 70px; line-height: 60px; border-radius: 5px">
+                    <h1 style="text-shadow: rgb(0, 0, 0) 0.1em 0.1em 0.2em"> Atendimento academico unidesc </h1>
                     
             </div>
             <div style="text-align: center; margin-bottom: 50px;">
-                    <h3>Esta mensagem foi enviada a partir do atendimento virtual do AgileBot </h3>
+                    <h3>Esta mensagem foi enviada a partir do atendimento virtual do AgileBot </h3><br>
             </div>
             <div style="text-align: left; margin-bottom: 50px;">
                     <p>Olá <b>${aluno.nome}</b>!<br></p>
-                    <p> Em anexo se encontra o documento solicitado de aluno.tipo no Atendiemento virtual</p><br><br>
+                    <p> Em anexo se encontra o documento solicitado de ${aluno.tipo} no Atendiemento virtual,<br>
+                            O centro Universitario Unidesc agradece o seu contato!
+                    </p><br><br><br>
                     <p>Atenciosamente,</p>
             </div>
             <div style="text-align: center;">
@@ -64,7 +67,7 @@ class DocumentoService {
                     </p>
             </div>
             <div style="text-align: center; margin-bottom: 100px;">
-                    <h4 style="background-color: rgba(12, 9, 9, 0.808);color: antiquewhite; height: 60px; line-height: 50px; border-radius: 5px"> Agile Bot® - 2021 Carlos Eduardo & Wanderson Rafael</h4>
+                    <h4 style="background-color: rgba(1, 65, 241, 0.808); text-shadow: rgb(0, 0, 0) 0.1em 0.1em 0.2em; color: antiquewhite; height: 60px; line-height: 50px; border-radius: 5px"> Agile Bot® - 2021 Carlos Eduardo & Wanderson Rafael</h4>
             </div>
         </div>
        `;
