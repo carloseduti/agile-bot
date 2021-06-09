2067685c3870a829324c2732dfa7c07e827c2bee
Wanderson027 <w.rafael.s@hotmail.com>
2021-06-08 22:00:51 -0300

    ajuste de mensagens

--- src/app/controllers/webHookController.js
+++ src/app/controllers/webHookController.js
@@ -19,11 +19,11 @@ class WebHookController {
       console.log("matricula -- >", matricula)
       const aluno = await new alunoService().findAlunoByMatricula(matricula);
       if (aluno) {
-        const textResponse = `Prezado ${aluno.nome}, seus dados foram localizados, deseja prosseguir com o atendimento?`;
+        const textResponse = `Prezado ${aluno.nome}, encontrei aqui seus dados, deseja prosseguir com o atendimento? \n\nDigite "Sim" para continuar ou "Não" caso deseje sair! `;
         const resultado = await new webHookService().createTextResponse(textResponse, 'matricula_encontrada_context', session);
         res.send(resultado);
       } else {
-        const textResponse = 'Infelizmente não encontramos sua matricula em nossa base de dados para continuar, escolha uma das opções: \n 1 - Tentar novamente \n 2 - Finalizar atendimento';
+        const textResponse = 'Infelizmente não encontramos sua matricula em nossa base de dados para continuar, escolha uma das opções: \n\n 1 - Tentar novamente \n 2 - Finalizar atendimento';
         const resultado = await new webHookService().createTextResponse(textResponse, 'tentar_novamente_context', session);
         res.send(resultado);
       }
@@ -42,7 +42,7 @@ class WebHookController {
     } else if (intent == 'declaracao') {
       const aluno = await new alunoService().findAlunoByMatricula('12345');
       if (aluno) {
-        const textResponse = `Entendido, foi enviado um email para: ${aluno.email}`
+        const textResponse = `Entendido, foi enviado um email para: ${aluno.email}, Deseja algo mais? \n\nDigite "Opções" para menu ou "Sair" caso deseje finalizar o atendimento!`
         const resultado = await new webHookService().createTextResponse(textResponse, 'tentar_novamente_context', session);
         await new secretariaService().enviarDocumentoPorTipo(aluno, tipoAtendimento.DECLARACAO)
         res.send(resultado);
