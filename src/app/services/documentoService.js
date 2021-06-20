c0fe7c4bf2f7f8e9ff900d076cfa8fddca18ffc4
Wanderson027 <w.rafael.s@hotmail.com>
2021-06-19 21:26:20 -0300

    ajuste html 4

--- src/app/services/documentoService.js
+++ src/app/services/documentoService.js
@@ -36,13 +36,13 @@ class DocumentoService {
         const conteudo = ` 
             <div style="margin: 0 auto; width: 90%;">
                 <div style="text-align: center; background-color: rgb(1, 120, 218); color: rgb(250, 250, 250); height: 70px; line-height: 60px; border-radius: 5px">
-                        <h1 style="font-size: 12px; text-shadow: rgb(32, 32, 32) 0.1em 0.1em 0.2em"> Atendimento Acadêmico Unidesc </h1>
+                        <h1 style="text-shadow: rgb(32, 32, 32) 0.1em 0.1em 0.2em"> Atendimento Acadêmico Unidesc </h1>
                         
                 </div>
-                <div style="text-align: center; margin-bottom: 50px;">
+                <div style="font-size: 12px; text-align: center; margin-bottom: 50px;">
                         <p style="font-size: 11px;"><b>Esta mensagem foi enviada a partir do atendimento virtual do AgileBot </b></p><br>
                 </div>
-                <div style="text-align: left; margin: 0 auto; width: 90%; margin-bottom: 50px;">
+                <div style="text-align: center; margin: 0 auto; width: 90%; margin-bottom: 50px;">
                         <p>Olá <b>${aluno.nome}</b>!<br></p>
                         <p> Em anexo se encontra o documento solicitado de <b>Declaração</b> no atendimento virtual.<br><br>
                                 O Unidesc Centro Universitário agradece o seu contato!
